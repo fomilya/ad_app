39a51ae3bfaa5b0701d32d0fb24f4caf89aa6f68
fomilya <fomilya@gmail.com>
2019-01-31 18:13:17 +0300

    tags template integration done

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,43 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+<div>
+  <label class="typo__label">Tagging</label>
+  <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag"></multiselect>
+  <pre class="language-json"><code>{{ value  }}</code></pre>
+</div>
 </template>
 
 <script>
+import Multiselect from 'vue-multiselect'
+
 export default {
-  data: function () {
+  components: {
+    Multiselect
+  },
+  data () {
     return {
-      message: "Hello Vue!"
+      value: [
+        { name: 'Javascript', code: 'js' }
+      ],
+      options: [
+        { name: 'Vue.js', code: 'vu' },
+        { name: 'Javascript', code: 'js' },
+        { name: 'Open Source', code: 'os' }
+      ]
+    }
+  },
+  methods: {
+    addTag (newTag) {
+      const tag = {
+        name: newTag,
+        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
+      }
+      this.options.push(tag)
+      this.value.push(tag)
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
